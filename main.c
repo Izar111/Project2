bf63c248e30800d45ed6d4662d9666ee08e340fc
lllminsuk <umsun3@naver.com>
2022-05-11 00:54:25 +0900

    함수 오타 수정

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ void filetime2(){
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    print("size compare\n");
+    printf("size compare\n");
     if(stat1.st_size>stat2.st_size) printf("text1 is bigger\n\n");
     else if(stat1.st_size<stat2.st_size) printf("text1 is bigger\n\n");
     else printf("size are equal\n\n");
@@ -57,7 +57,7 @@ void sizecmp(){
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    print("block compare\n");
+    printf("block compare\n");
     if(stat1.st_blocks>stat2.st_blocks) printf("text1 is bigger\n\n");
     else if(stat1.st_blocks<stat2.st_blocks) printf("text1 is bigger\n\n");
     else printf("size are equal\n\n");
